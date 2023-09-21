f0ab40d97800494139ccc81aed3d7b1c9700dd2e
pavanmanikrao-hyniva <pavan.manikrao@hyniva.com>
2023-09-21 13:32:14 +0530

    parser,parse

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const parser = require('lambda-multipart-parser');
 
 const uploadFile = async (event) => {
-    const { file, fields } = await parser(event);
+    const { file, fields } = await parser.parse(event);
     const tags = { filename: file.filename };
     try {
       await s3Client
